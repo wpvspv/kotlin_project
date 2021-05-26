34eacc674e29bb36ff09cc4cf7e4dd955230a9ac
wpvspv <wpvspv@gmail.com>
2021-05-27 00:08:45 +0900

    검색기능 추가

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -16,6 +16,11 @@ import androidx.databinding.DataBindingUtil
 import com.example.myapplication.databinding.ActivityMainBinding
 import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 val PERMISSIONS_REQUEST_CODE = 100
@@ -23,14 +28,28 @@ var REQUIRED_PERMISSIONS = arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCAT
 
 class MainActivity : AppCompatActivity() {
     //private lateinit var binding: ActivityMainBinding
+
+    companion object{
+        const val BASE_URL = "https://dapi.kakao.com/"
+        const val API_KEY = "2ec1e14fafbef8934e6f59d7e8dd8391"
+    }
+
+    private var keyword =""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding =
+       var binding =
             DataBindingUtil.setContentView(this, R.layout.activity_main) as ActivityMainBinding
 
         val mapView = MapView(this)
         binding.mapContainer.addView(mapView)
 
+        //검색 눌렀을 때
+        binding.searchBtn.setOnClickListener{
+            keyword =binding.searchBar.text.toString()
+            searchKeyword(keyword)
+        }
+
+        //현재 위치 확인 함수
         binding.mapPageLocationBtn.setOnClickListener {
             val permissionCheck =
                 ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
@@ -64,6 +83,32 @@ class MainActivity : AppCompatActivity() {
                 )
             }
         }
+    }
 
+    //키워드 검색 함수
+    private fun searchKeyword(keyword: String) {
+        val retrofit = Retrofit.Builder()   // Retrofit 구성
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val api = retrofit.create(KakaoAPI::class.java)   // 통신 인터페이스를 객체로 생성
+        val call = api.getSearchKeyword(API_KEY, keyword)   // 검색 조건 입력
+
+        // API 서버에 요청
+        call.enqueue(object: Callback<ResultSearchKeyword> {
+            override fun onResponse(
+                call: Call<ResultSearchKeyword>,
+                response: Response<ResultSearchKeyword>
+            ) {
+                // 통신 성공 (검색 결과는 response.body()에 담겨있음)
+                Log.d("Test", "Raw: ${response.raw()}")
+                Log.d("Test", "Body: ${response.body()}")
+            }
+
+            override fun onFailure(call: Call<ResultSearchKeyword>, t: Throwable) {
+                // 통신 실패
+                Log.w("MainActivity", "통신 실패: ${t.message}")
+            }
+        })
     }
 }
